246f5e833b098b3ba8b31228563c3a873845b864
Jose Alfonso Zamorano Raya <jose.zamorano16@tectijuana.edu.mx>
2018-12-08 08:09:33 -0800

    Update AssigmentLinearRegretion.scala

--- Unidad3/practicas/AssigmentLinearRegretion.scala
+++ Unidad3/practicas/AssigmentLinearRegretion.scala
@@ -1,24 +1,35 @@
-////////////////////////////////////////////
+///////////////////////////////////////////
 //// LINEAR REGRESSION EXERCISE ///////////
 /// Coplete las tareas comentadas ///
 /////////////////////////////////////////
 
 // Import LinearRegression
-
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.regression.LinearRegression
 // Opcional: Utilice el siguiente codigo para configurar errores
 import org.apache.log4j._
 Logger.getLogger("org").setLevel(Level.ERROR)
 
 
 // Inicie una simple Sesion Spark
-
+val spark = SparkSession.builder().getOrCreate()
 // Utilice Spark para el archivo csv Clean-Ecommerce .
+val data  = spark.read.option("header","true").option("inferSchema", "true").format("csv").load("Clean-Ecommerce.csv")
 
 // Imprima el schema en el DataFrame.
-
-
+data.printSchema
 // Imprima un renglon de ejemplo del DataFrane.
+data.head(1)
 
+val colnames = data.columns
+val firstrow = data.head(1)(0)
+println("\n")
+println("Example data row")
+for(ind <- Range(1, colnames.length)){
+    println(colnames(ind))
+    println(firstrow(ind))
+    println("\n")
+}
 
 
 //////////////////////////////////////////////////////
@@ -29,10 +40,12 @@ Logger.getLogger("org").setLevel(Level.ERROR)
 // ("label","features")
 
 // Importe VectorAssembler y Vectors
-
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 // Renombre la columna Yearly Amount Spent como "label"
-// Tambien de los datos tome solo la columa numerica 
+// Tambien de los datos tome solo la columa numerica
 // Deje todo esto como un nuevo DataFrame que se llame df
+val df = data.select(data("Yearly Amount Spent").as("label"), $"Avg Session Length", $"Time on App" , $"Time on Website",$"Length of Membership")
 
 // Que el objeto assembler convierta los valores de entrada a un vector
 
@@ -41,23 +54,29 @@ Logger.getLogger("org").setLevel(Level.ERROR)
 // a una sola columna de salida de un arreglo llamado  "features"
 // Configure las columnas de entrada de donde se supone que leemos los valores.
 // Llamar a esto nuevo assambler.
-
 // Utilice el assembler para transform nuestro DataFrame a dos columnas: label and features
-
+val assembler = new VectorAssembler().setInputCols(Array("Avg Session Length","Time on Website","Time on App","Length of Membership")).setOutputCol("features")
+val output = assembler.transform(df).select($"label", $"features")
 
 // Crear un objeto para modelo de regresion linea.
+val lr = new LinearRegression()
+val lrModel = lr.fit(output)
 
-
+output.show()
 // Ajuste el modelo para los datos y llame a este modelo lrModelo
-
-
+val lrModel = lr.fit(output)
+val trainingSummary = lrModel.summary
 // Imprima the  coefficients y intercept para la regresion lineal
 
 // Resuma el modelo sobre el conjunto de entrenamiento imprima la salida de algunas metricas!
+trainingSummary.residuals.show()
 // Utilize metodo .summary de nuestro  modelo para crear un objeto
 // llamado trainingSummary
-
+trainingSummary.predictions.show()
 // Muestre los valores de residuals, el RMSE, el MSE, y tambien el R^2 .
 
+
+trainingSummary.r2 //variaza que hay
+trainingSummary.rootMeanSquaredError
 
 // Buen trabajo!
